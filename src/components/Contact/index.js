0c59019ee488192322099c4c9853ca142a97ac05
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2023-09-06 18:49:23 +1200

    Do contact function.

---
+++ src/components/Contact/index.js
@@ -0,0 +1,72 @@
+import Loader from 'react-loaders'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser'
+
+const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const refForm = useRef()
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [])
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs
+            .sendForm('service_zh4gdjg', 'template_s4w5c2z', refForm.current, 'DPqxR6EzUYM-mVVXx')
+            .then(
+                () => {
+                    alert('Email sent successfully!');
+                    window.location.reload(false);
+                },
+                () => {
+                    alert('Email sent failed, please try again!');
+                }
+            )
+
+    }
+    return (
+        <>
+            <div className="container contact-page">
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters letterClass={letterClass} strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']} idx={15} />
+                    </h1>
+                    <p>
+                        Introduction something about me
+                    </p>
+                    <div className="contact-form">
+                        <form ref={refForm} onSubmit={sendEmail}>
+                            <ul>
+                                <li className='half'>
+                                    <input type="text" name="name" placeholder="Name" required />
+                                </li>
+                                <li className='half'>
+                                    <input type="email" name="email" placeholder="Email" required />
+                                </li>
+                                <li >
+                                    <input type="text" name="subject" placeholder="Subject" required />
+                                </li>
+                                <li>
+                                    <textarea name="message" placeholder="Message" required></textarea>
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    )
+}
+
+export default Contact
