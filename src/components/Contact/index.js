e3056b3b1ba74cafdeafdf34d20a3bc7ba2cafab
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2023-09-08 16:07:58 +1200

    Add map feature but have bugs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,6 +3,7 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters';
 import { useEffect, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
@@ -62,6 +63,26 @@ const Contact = () => {
                             </ul>
                         </form>
                     </div>
+                </div>
+                <div className="info-map">
+                    Ralph Li,
+                    <br />
+                    Newzealand,
+                    <br />
+                    128 Anzac Avenue, Auckland CBD,
+                    <br />
+                    Auckland <br />
+                    <span >lzh8612@outlook.com</span>
+                </div>
+                <div className='map-wrap'>
+                    <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+                        <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+                        <Marker position={[44.96366, 19.61045]} >
+                            <Popup>
+                                Ralph lives here, come over for a coffee :)
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>
             <Loader type="pacman" />
