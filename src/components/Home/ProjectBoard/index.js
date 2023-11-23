2cd93a7591c59044ed5fbcaf5110bf44623ea569
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2023-11-23 18:39:49 +1300

    Remove DrawSVGPlugin.

--- src/components/Home/ProjectBoard/index.js
+++ src/components/Home/ProjectBoard/index.js
@@ -1,6 +1,6 @@
 import './index.scss'
 import { useEffect, useRef } from 'react'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import gsap from 'gsap-trial'
 import ProjectFrame from './ProjectFrame'
 import blogPage from '../../../assets/images/blogPage.png'
@@ -10,20 +10,20 @@ const ProjectBoard = () => {
   const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-    gsap
-      .timeline()
-      .to(bgRef.current, { duration: 1, opacity: 1 })
-      .from(outlineLogoRef.current, { drawSVG: 0, duration: 20 })
-    gsap.fromTo(
-      solidLogoRef.current,
-      { opacity: 0 },
-      { opacity: 1, duration: 4, delay: 4 }
-    )
-  }, [])
+  // useEffect(() => {
+  //   gsap.registerPlugin(DrawSVGPlugin)
+  //   gsap
+  //     .timeline()
+  //     .to(bgRef.current, { duration: 1, opacity: 1 })
+  //     .from(outlineLogoRef.current, { drawSVG: 0, duration: 20 })
+  //   gsap.fromTo(
+  //     solidLogoRef.current,
+  //     { opacity: 0 },
+  //     { opacity: 1, duration: 4, delay: 4 }
+  //   )
+  // }, [])
   return (
-    <div className="logo-container" ref={bgRef}>
+    <div className="logo-container">
       <div className="topRow">
         <ProjectFrame
           url="https://google-search-demo-lovat.vercel.app/"
