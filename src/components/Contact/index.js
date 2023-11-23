c200c1b984714e8fba434ef86d356bb88e783add
Zhaohua LI <127060502+zli213@users.noreply.github.com>
2023-11-23 20:44:26 +1300

    Optimise of Contact page.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,96 +1,114 @@
 import Loader from 'react-loaders'
 import './index.scss'
-import AnimatedLetters from '../AnimatedLetters';
-import { useEffect, useRef, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Contact = () => {
-    const [letterClass, setLetterClass] = useState('text-animate');
-    const refForm = useRef()
-    useEffect(() => {
-        const timerId = setTimeout(() => {
-            setLetterClass('text-animate-hover');
-        }, 3000);
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
 
-        return () => {
-            clearTimeout(timerId);
-        };
-    }, [])
-    const sendEmail = (e) => {
-        e.preventDefault();
-        emailjs
-            .sendForm('default_service', 'template_s4w5c2z', refForm.current, 'DPqxR6EzUYM-mVVXx')
-            .then(
-                () => {
-                    alert('Email sent successfully!');
-                    window.location.reload(false);
-                },
-                () => {
-                    alert('Email sent failed, please try again!');
-                }
-            )
-
+    return () => {
+      clearTimeout(timerId)
     }
-    return (
-        <>
-            <div className="container contact-page">
-                <div className='text-zone'>
-                    <h1>
-                        <AnimatedLetters letterClass={letterClass} strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']} idx={15} />
-                    </h1>
-                    <p>
-                        Introduction something about me
-                    </p>
-                    <div className="contact-form">
-                        <form ref={refForm} onSubmit={sendEmail}>
-                            <ul>
-                                <li className='half'>
-                                    <input type="text" name="name" placeholder="Name" required />
-                                </li>
-                                <li className='half'>
-                                    <input type="email" name="email" placeholder="Email" required />
-                                </li>
-                                <li >
-                                    <input type="text" name="subject" placeholder="Subject" required />
-                                </li>
-                                <li>
-                                    <textarea name="message" placeholder="Message" required></textarea>
-                                </li>
-                                <li>
-                                    <input type="submit" className="flat-button" value="SEND" />
-                                </li>
-                            </ul>
-                        </form>
-                    </div>
-                </div>
-                <div className="info-map">
-                    Ralph Li,
-                    <br />
-                    Newzealand,
-                    <br />
-                    128 Anzac Avenue, Auckland CBD,
-                    <br />
-                    Auckland <br />
-                    <span >lzh8612@outlook.com</span>
-                </div>
-                <div className='map-wrap'>
-                    <MapContainer center={[-36.8491281, 174.7713053]} zoom={13}>
-                        <TileLayer
-                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                        <Marker position={[-36.8491281, 174.7713053]}>
-                            <Popup>
-                                Ralph lives here, come over for a coffee
-                            </Popup>
-                        </Marker>
-                    </MapContainer>
-                </div>
-            </div>
-            <Loader type="pacman" />
-        </>
-    )
+  }, [])
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs
+      .sendForm(
+        'default_service',
+        'template_s4w5c2z',
+        refForm.current,
+        'DPqxR6EzUYM-mVVXx'
+      )
+      .then(
+        () => {
+          alert('Email sent successfully!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Email sent failed, please try again!')
+        }
+      )
+  }
+  return (
+    <>
+      <div className={'contact-page'}>
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>Introduction something about me</p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    name="message"
+                    placeholder="Message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="info-map">
+          Ralph Li,
+          <br />
+          Newzealand,
+          <br />
+          128 Anzac Avenue, Auckland CBD,
+          <br />
+          Auckland <br />
+          <span>lzh8612@outlook.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[-36.8491281, 174.7713053]} zoom={13}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[-36.8491281, 174.7713053]}>
+              <Popup>Ralph lives here, come over for a coffee</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Contact
